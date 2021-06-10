306e003737711c9ff30794a134fa9a9598daf61e
HARDIK DOBARIYA <dobariyahardik000@gmail.com>
2021-06-10 10:55:45 +0530

    function modularization and conting sactter

--- src/controller/game.js
+++ src/controller/game.js
@@ -17,67 +17,52 @@ class SlotGame {
                 return response;
             }else {
                 let wallet = reslt.content.wallet;
-                console.log(wallet);
                 let betAmount = reslt.content.betAmount;
                 let freeSpin = reslt.content.freeSpin;
                 let WinFreeSpinAmount = reslt.content.WinFreeSpinAmount;
                 let totalfreeSpin = reslt.content.totalfreeSpin; 
-                
-                const randomNumber = gameHelper.randomInt(0,9);
                 getObject("MyJackpot").then((gameVariable) =>{
-
-                    
-                    const generateViewZone = gameHelper.generateViewZone(randomNumber,gameVariable.content.static);
+                    // Generate ViewZone
+                    const generateViewZone = gameHelper.generateViewZone(gameVariable.content.static);
                     const viewZone = generateViewZone.viewZone;
                     
                     //create Reel X colume matrix
                     let matrixReelXCol = gameHelper.matrix(generateViewZone,gameVariable.content.static.viewZone.rows , gameVariable.content.static.viewZone.columns)
                     //console.log(matrixReelXCol);
 
-                    let sactterCount = 0; 
                     // in matrix check payline available 
                     let checkPayline = gameHelper.checkPayline(gameVariable.content.static.payarray,matrixReelXCol,reslt.content,gameVariable.content.static.payTable);
-                    console.log(checkPayline);
-                    sactterCount = checkPayline.sactterCount;
-                    console.log(checkPayline.wallet + "   this is wallet");
-                    let result =checkPayline.result[0];
-                    if (!checkPayline.wallet){
-                        wallet = checkPayline.wallet;
-                        WinFreeSpinAmount = checkPayline.WinFreeSpinAmount;
-                    }
-                    
-                    // free spin counting and free spin not occurred
-                    if(freeSpin != 0){
+                    WinFreeSpinAmount = checkPayline.WinFreeSpinAmount;
+                    wallet = checkPayline.wallet
+                     // free spin counting and free spin not occurred
+                     if(freeSpin !== 0){
                         freeSpin--;
                     }else{
-                        wallet = gameHelper.debitWinAmount(wallet,reslt.content.betAmount);
+                        wallet = gameHelper.debitWinAmount(checkPayline.wallet,reslt.content.betAmount);
                         WinFreeSpinAmount =0;
                     }
-                    
-                    //when free spin given
-                
-                    if (sactterCount > 2) {
-                        if(freeSpin > 0){
-                            totalfreeSpin += 3 ;
-                            freeSpin += 3; 
-                        }else{
-                            freeSpin =5 ;
-                            totalfreeSpin = freeSpin;
-                        }
-                        
-                    }
 
-                    let scatterOffreeSpin = gameHelper.freeSpin(freeSpin,WinFreeSpinAmount,totalfreeSpin);
+                    //when free spin given
+                    if (checkPayline.sactterCount > 2) {
+                        let countOfFreeSpin = gameHelper.countOfFreeSpin(freeSpin,totalfreeSpin);
+                        freeSpin = countOfFreeSpin.freeSpin;
+                        totalfreeSpin = countOfFreeSpin.totalfreeSpin;
+                    }
+                    let responceFreeSpin = {};
+                    if (freeSpin === 0) {
+                        responceFreeSpin = gameHelper.freeSpin(checkPayline.freeSpin,checkPayline.WinFreeSpinAmount,checkPayline.totalfreeSpin);
+                    }else {
+                        responceFreeSpin = gameHelper.freeSpin(freeSpin,checkPayline.WinFreeSpinAmount,totalfreeSpin);
+                    }
+                    
+                    let result =checkPayline.result;
                     
                     reslt.content.wallet = wallet;
                     reslt.content.betAmount = betAmount;
                     reslt.content.freeSpin = freeSpin;
                     reslt.content.WinFreeSpinAmount = WinFreeSpinAmount;
                     reslt.content.totalfreeSpin = totalfreeSpin;
-                    couchbaseCollection.upsert( reslt.content.email,reslt.content);
-                    upsertObject(reslt.content.email,reslt.content).then(()=>{
-
-                    }).catch(err => {
+                    upsertObject(reslt.content.email,reslt.content).then(()=>{}).catch(err => {
                         let response = falshMessage.resDispatchError(res,'NOT_FOUND');
                         return response;
                     });
@@ -86,11 +71,10 @@ class SlotGame {
                         result    : result,
                         betAmount : betAmount, 
                         wallet    : wallet,
-                        freeSpin  : freeSpin > 0 ? scatterOffreeSpin : 0 
+                        freeSpin  : checkPayline.freeSpin > 0 ? responceFreeSpin : 0  
                     }
                     let response = falshMessage.resDispatch(res,'OK',data);
-                    return response;
-                    
+                    return response; 
                 }) 
             }
         });
